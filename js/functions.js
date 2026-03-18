35ddbb4c1b699a4aab00dc7c10411dc8ad8f1edc
jmanuel0708 <josefo.juarez@gmail.com>
2026-03-18 11:43:38 -0500

    Remove button enabled

--- js/functions.js
+++ js/functions.js
@@ -5,12 +5,23 @@ const cart = document.querySelector(".cart")
 cartIcon.addEventListener("click", () => {
     
     if (!cart.classList.contains("cart__show")) {
-        cart.classList.add("cart__show")      
+        cart.classList.add("cart__show");   
     }
 })
 
 closeCartIcon.addEventListener("click", () => {
     if (cart.classList.contains("cart__show")) {
-        cart.classList.remove("cart__show")
+        cart.classList.remove("cart__show");
     }
+})
+
+//
+
+const iconRemove = document.querySelectorAll(".cart__article__btn");
+
+iconRemove.forEach(elem => {
+    elem.addEventListener("click", () => {
+        const elemParent = elem.parentElement;
+        elemParent.remove();
+    })
 })
