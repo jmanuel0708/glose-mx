de2c7bde5554cb86f91b02db8cca03eb22b5d9e3
jmanuel0708 <josefo.juarez@gmail.com>
2026-03-18 13:29:45 -0500

    Add to bag now inserts the item to the cart

--- js/functions.js
+++ js/functions.js
@@ -33,3 +33,47 @@ iconRemove.forEach(elem => {
 
 // Add item to cart
 
+const addToBag = document.querySelectorAll(".card__btn");
+
+addToBag.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const card = e.currentTarget.closest(".card");
+
+        const img = card.querySelector(".card__img");
+        const title = card.querySelector(".card__item");
+        const price = card.querySelector(".card__price");
+
+        const imgSrc = img.getAttribute("src");
+        const imgAlt = img.getAttribute("alt");
+        const titleText = title.textContent;
+        const priceText = price.textContent;
+
+        const newArticleInCart = document.createElement("article");
+        newArticleInCart.setAttribute("class","cart__article");
+        cart.append(newArticleInCart);
+
+        const newImg = document.createElement("img");
+        newImg.setAttribute("class", "cart__article__img");
+        newImg.setAttribute("src", imgSrc);
+        newImg.setAttribute("alt", imgAlt);
+        
+        const newTitle = document.createElement("h3");
+        newTitle.setAttribute("class", "cart__article__item");
+        newTitle.textContent = titleText;
+
+        const newPrice = document.createElement("p");
+        newPrice.setAttribute("class", "cart__article__price");
+        newPrice.textContent = priceText;
+
+        const newBtn = document.createElement("img");
+        newBtn.setAttribute("class", "cart__article__btn");
+        newBtn.setAttribute("src","img/icons/trash.svg");
+        newBtn.setAttribute("alt", "Remove from cart");
+
+        
+        newArticleInCart.append(newImg);
+        newArticleInCart.append(newTitle);
+        newArticleInCart.append(newPrice);
+        newArticleInCart.append(newBtn);
+    })
+})
